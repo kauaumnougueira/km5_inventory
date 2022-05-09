e0fcb2e47713e00a4930c8cca9b1e0bdfd388e50
kauaumonougueira <kaua_kna@hotmail.com>
2022-05-09 15:08:57 -0300

    Enums 'services' criado + objetos de 'client' criados

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,5 +1,48 @@
 package entities;
 
+import entities.enums.Services;
+
 public class Client {
+    private String name;
+    private Integer number;
+    private String address;
+    private Double price;
+    private Services services;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    } 
+
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    } 
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    } 
+
+
+    public Double price() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    } 
+
+    public Services getServices() {
+        return services;
+    }
+    public void setServices(Services services) {
+        this.services = services;
+    }
     
 }
---
+++ src/entities/enums/Services.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum Services {
+    LIMPEZA,
+    MANUTENÇÃO,
+    INSTALAÇÃO,
+    DESINSTALAÇÃO,
+    OUTROS;
+}
