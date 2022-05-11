28800d4215ce550ce6a008e211b3de0e63e376c8
kauaumonougueira <kaua_kna@hotmail.com>
2022-05-11 11:34:43 -0300

    erro de exception corrigido :D

--- src/gui/layoutController.java
+++ src/gui/layoutController.java
@@ -11,12 +11,32 @@ import javafx.scene.Scene;
 public class layoutController extends Program{
     @FXML
     private Button add;
+    @FXML
+    private Button toSee;
+    @FXML
+    private Button change;
+
+    @FXML
+    public void onChangeAction() throws Exception{
+        onAction(add, "ClientAdd.fxml", "ADCIONANDO NOVOS SERVIÇOS"); 
+    }
+    
+    @FXML
+    public void onAction(Button e, String path, String title) throws Exception{
+        deleteFrame(toSee);
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+        Parent root = loader.load();
+        Scene tela = new Scene(root);
+         
+        stage.setTitle(title);
+        stage.setScene(tela);
+        stage.show();
+    }
 
     @FXML
     public void onAddAction()throws Exception{
-        //fechando a tela atual
-        Stage stageAtual = (Stage) add.getScene().getWindow(); //Obtendo a janela atual
-        stageAtual.close();
+        deleteFrame(add);
 
         Stage stage = new Stage();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ClientAdd.fxml"));
@@ -26,5 +46,11 @@ public class layoutController extends Program{
         stage.setTitle("ADCIONANDO NOVOS SERVIÇOS");
         stage.setScene(tela);
         stage.show();
+    }
+    
+    public void deleteFrame(Button e){
+        //fechando a tela atual
+        Stage stageAtual = (Stage) e.getScene().getWindow(); //Obtendo a janela atual
+        stageAtual.close();
     }
 }
